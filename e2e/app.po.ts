136922c729d339e88964c262f556466a955b1024
Juan Narvaez <juan.narvaez@correounivall.edu.co>
2017-06-27 00:45:38 -0500

    CLI angular

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class MovieCenterPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
